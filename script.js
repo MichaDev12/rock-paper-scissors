42ce4e880439e6160f657db8d37db5a91aa49232
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-01-04 16:42:34 -0500

    Fix the way when the random number is 0 using a do-while
    I also add differents numbers to get random results :D

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 function getComputerChoice() {
-  let randomNumber = Math.trunc(Math.random() * 10);
+  let randomNumber;
   let choice;
 
-  // I add this because i want to work with 1-3, 4-6 and 7-9
-  if (randomNumber === 0) {
+  do {
     randomNumber = Math.trunc(Math.random() * 10);
-  }
+  } while (randomNumber === 0);
 
   switch (randomNumber) {
-    // If i have the 0 again haha (this will change in the future I promise D:)
-    case 0:
-      randomNumber = Math.trunc(Math.random() * 10);
     case 1:
-    case 2:
-    case 3:
+    case 4:
+    case 7:
       choice = "Rock";
       break;
-    case 4:
+    case 2:
     case 5:
-    case 6:
+    case 8:
       choice = "Paper";
       break;
-    case 7:
-    case 8:
+    case 3:
+    case 6:
     case 9:
       choice = "scissors";
       break;
-    // I add this if there is a possibility to get a triple zero, this will be fix with loops soon
-    default:
-      choice = '0 0 0 :('
   }
 
   return choice;
